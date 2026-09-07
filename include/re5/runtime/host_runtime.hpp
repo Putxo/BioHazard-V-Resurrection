217a10159bb97d1b6e4f8856a46053e8e1e05844
Putxo <putxovitx@protonmail.com>
2026-09-07 23:53:09 +0200

    runtime: expose Win32 window-title adapter

--- include/re5/runtime/host_runtime.hpp
+++ include/re5/runtime/host_runtime.hpp
@@ -8,4 +8,8 @@ namespace re5::runtime {
 void aligned_release(void* ptr) noexcept;
 void debug_output_a(const char* text) noexcept;
 
+// Portable representation of the target's FindWindowW(class_name, nullptr) +
+// SetWindowTextW(hwnd, title) side effect. On the Win32 target wchar_t is 16-bit.
+void set_window_title_for_class(const char16_t* class_name, const char16_t* title) noexcept;
+
 } // namespace re5::runtime
