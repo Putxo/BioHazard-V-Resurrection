d5dcc8005dfde6480f33eb6d1e4adf20dba18acf
Putxo <putxovitx@protonmail.com>
2026-09-10 22:12:55 +0200

    Decompile FUN_00402000 constant thunk block

---
+++ src/recovered/fun_00402000.cpp
@@ -0,0 +1,13 @@
+#include "re5/recovered/fun_00402000.hpp"
+
+namespace re5::recovered {
+
+std::uintptr_t FUN_00402000() noexcept {
+    return 0x016E6EC0U;
+}
+
+std::uint32_t FUN_00402010() noexcept {
+    return 0x98U;
+}
+
+} // namespace re5::recovered
--- tests/unit/test_smoke.cpp
+++ tests/unit/test_smoke.cpp
@@ -28,6 +28,7 @@ void test_fun_00401d50();
 void test_fun_00401e30();
 void test_fun_00401e60();
 void test_fun_00401fe0();
+void test_fun_00402000();
 
 int main() {
     if (re5::recovered_runtime_abi_version() != 1U) return 1;
@@ -55,5 +56,6 @@ int main() {
     test_fun_00401e30();
     test_fun_00401e60();
     test_fun_00401fe0();
+    test_fun_00402000();
     return 0;
 }
