4d988648dde21afde3b62865e09a12f0b18602db
Putxo <putxovitx@protonmail.com>
2026-09-10 15:42:13 +0200

    Add FUN_00401830 recovered interface

---
+++ include/re5/recovered/fun_00401830.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+
+namespace re5::recovered {
+
+[[nodiscard]] bool FUN_00401830(
+    const std::uint8_t* first,
+    const std::uint8_t* second,
+    std::uint32_t length) noexcept;
+
+} // namespace re5::recovered
