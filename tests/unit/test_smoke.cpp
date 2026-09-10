8ce7cd535478ca8674dcc18292dea8a0a16c6b3f
Putxo <putxovitx@protonmail.com>
2026-09-10 22:10:43 +0200

    Decompile FUN_00401FE0 formatting wrapper

--- tests/unit/test_smoke.cpp
+++ tests/unit/test_smoke.cpp
@@ -27,6 +27,7 @@ void test_fun_00401d10();
 void test_fun_00401d50();
 void test_fun_00401e30();
 void test_fun_00401e60();
+void test_fun_00401fe0();
 
 int main() {
     if (re5::recovered_runtime_abi_version() != 1U) return 1;
@@ -53,5 +54,6 @@ int main() {
     test_fun_00401d50();
     test_fun_00401e30();
     test_fun_00401e60();
+    test_fun_00401fe0();
     return 0;
 }
