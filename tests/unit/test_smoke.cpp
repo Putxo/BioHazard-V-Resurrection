b8e71c261a4d1a6ebfdf1f7ab107c45a47e86a95
Putxo <putxovitx@protonmail.com>
2026-09-10 13:49:46 +0200

    Run FUN_004015D0 smoke coverage

--- tests/unit/test_smoke.cpp
+++ tests/unit/test_smoke.cpp
@@ -7,11 +7,13 @@ std::uint32_t recovered_runtime_abi_version() noexcept;
 void test_pilot_bb9070_accessors();
 void test_region_00401000();
 void test_fun_004014c0();
+void test_fun_004015d0();
 
 int main() {
     if (re5::recovered_runtime_abi_version() != 1U) return 1;
     test_pilot_bb9070_accessors();
     test_region_00401000();
     test_fun_004014c0();
+    test_fun_004015d0();
     return 0;
 }
