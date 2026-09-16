ea78cc692317c9f9f8f69d557eb69b1bb5f2f724
Putxo <putxovitx@protonmail.com>
2026-09-16 13:54:58 +0200

    Decompile 00402480 utility block

--- tests/unit/test_smoke.cpp
+++ tests/unit/test_smoke.cpp
@@ -35,6 +35,7 @@ void test_fun_00402110();
 void test_fun_00402120();
 void test_fun_00402250();
 void test_fun_004022f0();
+void test_fun_00402480();
 
 int main() {
     if (re5::recovered_runtime_abi_version() != 1U) return 1;
@@ -69,5 +70,6 @@ int main() {
     test_fun_00402120();
     test_fun_00402250();
     test_fun_004022f0();
+    test_fun_00402480();
     return 0;
 }
