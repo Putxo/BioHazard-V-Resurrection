89871357fbe273f1b996cf7341f7fb639756c700
Putxo <putxovitx@protonmail.com>
2026-09-21 23:10:09 +0200

    Run FUN_00405BD0 unit coverage

--- tests/unit/test_smoke.cpp
+++ tests/unit/test_smoke.cpp
@@ -45,6 +45,7 @@ void test_fun_00403f50();
 void test_fun_00404e60();
 void test_fun_00404f90();
 void test_fun_00405710();
+void test_fun_00405bd0();
 
 int main() {
     if (re5::recovered_runtime_abi_version() != 1U) return 1;
@@ -89,5 +90,6 @@ int main() {
     test_fun_00404e60();
     test_fun_00404f90();
     test_fun_00405710();
+    test_fun_00405bd0();
     return 0;
 }
