b89d19e11fe09ca55a85428a6d6a598f58ecccc8
Putxo <putxovitx@protonmail.com>
2026-09-22 18:46:58 +0200

    Add FUN_004067E0 interface

---
+++ include/re5/recovered/fun_004067e0.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+
+namespace re5::recovered {
+struct FUN_004067E0_Services {
+    void* context;
+    void (*dispatch_resource_group)(void*, std::int32_t, const char*, std::uintptr_t, std::int32_t, std::uintptr_t, std::int32_t) noexcept;
+};
+void FUN_004067E0_SetServices(const FUN_004067E0_Services* services) noexcept;
+void FUN_004067E0() noexcept;
+} // namespace re5::recovered
