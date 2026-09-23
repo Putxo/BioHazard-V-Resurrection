e42331a8c842831f8e331eef6847714f53099b95
Putxo <putxovitx@protonmail.com>
2026-09-23 18:40:16 +0000

    Publish FUN_00406B20 fast pass

--- tests/unit/test_smoke.cpp
+++ tests/unit/test_smoke.cpp
@@ -80,6 +80,7 @@ void test_fun_00406840();
 void test_fun_004068a0();
 
 void test_fun_00406910();
+void test_fun_00406b20();
 int main() {
     if (re5::recovered_runtime_abi_version() != 1U) return 1;
     test_pilot_bb9070_accessors();
@@ -154,5 +155,6 @@ int main() {
     test_fun_00406840();
     test_fun_004068a0();
     test_fun_00406910();
+    test_fun_00406b20();
     return 0;
 }
