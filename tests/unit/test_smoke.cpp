1c54ee355ac4815f753abd4f30df79949953c861
Putxo <putxovitx@protonmail.com>
2026-09-24 05:22:57 +0200

    Run FUN_00408490 in public smoke test

--- tests/unit/test_smoke.cpp
+++ tests/unit/test_smoke.cpp
@@ -111,6 +111,7 @@ void test_fun_00408070();
 void test_fun_00408100();
 void test_fun_004082e0();
 void test_fun_004083c0();
+void test_fun_00408490();
 int main() {
     if (re5::recovered_runtime_abi_version() != 1U) return 1;
     test_pilot_bb9070_accessors();
@@ -216,5 +217,6 @@ int main() {
     test_fun_00408100();
     test_fun_004082e0();
     test_fun_004083c0();
+    test_fun_00408490();
     return 0;
 }
