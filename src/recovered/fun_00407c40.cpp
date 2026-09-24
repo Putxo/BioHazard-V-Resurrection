2f72e05554e19acea24a41703354903a19497581
Putxo <putxovitx@protonmail.com>
2026-09-24 03:17:00 +0200

    Publish validated FUN_00407C40 dashboard

---
+++ src/recovered/fun_00407c40.cpp
@@ -0,0 +1,12 @@
+#include "re5/recovered/fun_00407c40.hpp"
+
+namespace re5::recovered {
+
+FUN_00407C40_Object* FUN_00407C40(
+    FUN_00407C40_Object& destination,
+    const char* input) noexcept {
+    FUN_00402360(destination, input);
+    return &destination;
+}
+
+} // namespace re5::recovered
