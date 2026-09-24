9a06fdf42929c026faa8454c3249bf07e79ffec9
Putxo <putxovitx@protonmail.com>
2026-09-24 04:32:21 +0200

    Recover FUN_00408040: include/re5/recovered/fun_00408040.hpp

---
+++ include/re5/recovered/fun_00408040.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "re5/recovered/fun_00407fd0.hpp"
+
+namespace re5::recovered {
+
+[[nodiscard]] FUN_00402360_String* FUN_00408040(
+    const FUN_00402360_String& source,
+    FUN_00402360_String& destination,
+    const char* suffix) noexcept;
+
+} // namespace re5::recovered
