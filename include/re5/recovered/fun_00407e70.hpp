2e3e649e1ca1ea78cb1fa98f408b0d4b4a741c0f
Putxo <putxovitx@protonmail.com>
2026-09-24 04:13:57 +0200

    Recover FUN_00407E70: add include/re5/recovered/fun_00407e70.hpp

---
+++ include/re5/recovered/fun_00407e70.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+
+namespace re5::recovered {
+
+struct FUN_00407E70_Object {
+};
+
+void* FUN_00407E70(
+    const FUN_00407E70_Object& object,
+    std::uint32_t requested_size);
+
+} // namespace re5::recovered
